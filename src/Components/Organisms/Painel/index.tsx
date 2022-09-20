513a01ac69990e8ed3b0f218b7e006662adb2513
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-09-20 04:14:22 -0300

    feat: change style & add edit exit button

--- src/Components/Organisms/Painel/index.tsx
+++ src/Components/Organisms/Painel/index.tsx
@@ -4,48 +4,59 @@ import PainelItem from '../../Molecules/PainelItem';
 import Icon from '../../utils/icon';
 import style from './style.module.css';
 
-
-
 export default function Painel() {
-
   const [data, setData] = useState([]);
   const [optionsVisibility, setOptionsVisibility] = useState(false);
   const [itemEdit, setItemEdit] = useState(false);
   const [update, setUpdate] = useState<string>('');
+
   useEffect(() => {
-    getData()
-  }, [update])
+    getData();
+  }, [update]);
 
-
-  async function getData(){
+  async function getData() {
     const database = await db();
 
-    const data = await database.getAll('website')as any;
-    setData(data)
+    const data = (await database.getAll('website')) as any;
+    setData(data);
   }
 
-  function toggleItensEdit(){
-    setOptionsVisibility(false)
-    setItemEdit(!itemEdit)
+  function toggleItensEdit() {
+    setOptionsVisibility(!optionsVisibility);
+    setItemEdit(!itemEdit);
   }
 
-  function PainelOption(){
+  function PainelOption() {
     return (
       <ul className={style.painelOption_suspend}>
-        <li className={style.painelOption_item}><button onClick={toggleItensEdit}>Editar</button></li>
-        <li className={style.painelOption_item}><button>Configurações</button></li>
+        <li className={style.painelOption_item}>
+          <button onClick={toggleItensEdit}>Editar</button>
+        </li>
+        <li className={style.painelOption_item}>
+          <button>Configurações</button>
+        </li>
       </ul>
-    )
+    );
+  }
+
+  function handleModeEdit(){
+    if(itemEdit){
+      toggleItensEdit()
+    }else{
+      setOptionsVisibility(!optionsVisibility)
+    }
   }
 
   return (
     <main className={style.painel}>
       <header className={style.header}>
         <div className={style.painelOptions}>
-          <button onClick={() => setOptionsVisibility(!optionsVisibility)}
-          data-focus={optionsVisibility} 
-          className={style.buttonToggleOption}>
-            <Icon name='dots' />
+          <button
+            onClick={() => handleModeEdit()}
+            data-focus={optionsVisibility}
+            className={style.buttonToggleOption}
+          >
+            {itemEdit ? <Icon name='arrow_left' /> : <Icon name='dots' />}
           </button>
           {optionsVisibility && <PainelOption />}
         </div>
@@ -62,16 +73,27 @@ export default function Painel() {
         </div>
       </header>
 
-
       <section className={style.section}>
-        {data && data.map((item: any, index:number) => {
-          return (<PainelItem onClick={setUpdate} edit={itemEdit} isDark={item.darkType}key={index} url_image={item.url_favicon} title={item.title} url={item.url}/>)
-        })}
+        {data &&
+          data.map((item: any, index: number) => {
+            return (
+              <PainelItem
+                onClick={setUpdate}
+                edit={itemEdit}
+                isDark={item.darkType}
+                key={index}
+                url_image={item.url_favicon}
+                title={item.title}
+                url={item.url}
+              />
+            );
+          })}
       </section>
 
-      
       <footer className={style.footer}>
-        <button className={style.buttonNotify}><Icon name='bell' /></button>
+        <button className={style.buttonNotify}>
+          <Icon name='bell' />
+        </button>
         <hr />
       </footer>
     </main>

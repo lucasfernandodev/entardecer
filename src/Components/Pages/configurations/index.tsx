13c6f183b2ab864a39ca4a4fc64d5e0171f9233f
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-09-21 18:47:15 -0300

    feat: Add page config

--- src/Components/Pages/configurations/index.tsx
+++ src/Components/Pages/configurations/index.tsx
@@ -1,55 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react';
 import Layout from '../../Atoms/Layout';
+import {SettingsInterfaceMenu } from '../../Molecules/SettingsInterfaceMenu';
 import style from './style.module.css';
 
 export default function Configurations() {
+
+  const [option, setOption] = useState<string>('painel');
+  const MenuRef = useRef<null | HTMLUListElement>(null);
+
+  useEffect(() => {
+    if(MenuRef.current){
+      MenuRef.current.querySelectorAll('a').forEach(e => e.removeAttribute('data-active'));
+      const activeLink = MenuRef.current.querySelector(`a[data-label=${option}]`);
+      activeLink?.setAttribute("data-active", 'true')
+    }
+  }, [option])
+
+  function selectOption(evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+    evt.preventDefault();
+    const element = evt.target as HTMLLinkElement;
+    const currentLabel = element.getAttribute('data-label');
+    setOption(currentLabel as string)
+  }
+
   return (
     <Layout large='full'>
       <div className={style.container}>
-        <header>
-          <h1>Configurações</h1>
-          <p>Todas as configurações são definidas por aqui</p>
+        <header className={style.header}>
+          <div className={style.brand}>
+            <img src='/images/logo.svg' alt='Logo entardecer' />
+            <h1>Entardecer</h1>
+          </div>
+          <ul className={style.menu} ref={MenuRef}>
+            <li className={style.menuItem}>
+              <a href='#' className={style.menuLink} onClick={selectOption} data-label="painel">
+                Painel
+              </a>
+            </li>
+            <li className={style.menuItem}>
+              <a href='#' className={style.menuLink} onClick={selectOption} data-label="interface">
+                Interface
+              </a>
+            </li>
+            <li className={style.menuItem}>
+              <a href='#' className={style.menuLink} onClick={selectOption} data-label="autoload">
+                autoload
+              </a>
+            </li>
+            <li className={style.menuItem}>
+              <a href='#' className={style.menuLink} onClick={selectOption} data-label="categorias">
+                Categorias
+              </a>
+            </li>
+          </ul>
         </header>
-        <section>
-          <details>
-            <summary><svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M3 21v-4a4 4 0 1 1 4 4h-4" />
-  <path d="M21 3a16 16 0 0 0 -12.8 10.2" />
-  <path d="M21 3a16 16 0 0 1 -10.2 12.8" />
-  <path d="M10.6 9a9 9 0 0 1 4.4 4.4" />
-</svg> Tema</summary>
-            <p>
-              Epcot is a theme park at Walt Disney World Resort featuring
-              exciting attractions, international pavilions, award-winning
-              fireworks and seasonal special events.
-            </p>
-          </details>
-          <details>
-            <summary>Background</summary>
-            <p>
-              Epcot is a theme park at Walt Disney World Resort featuring
-              exciting attractions, international pavilions, award-winning
-              fireworks and seasonal special events.
-            </p>
-          </details>
-          <details>
-            <summary>Categorias</summary>
-            <p>
-              Epcot is a theme park at Walt Disney World Resort featuring
-              exciting attractions, international pavilions, award-winning
-              fireworks and seasonal special events.
-            </p>
-          </details>
-          <details>
-            <summary>Autoload</summary>
-            <p>
-              Epcot is a theme park at Walt Disney World Resort featuring
-              exciting attractions, international pavilions, award-winning
-              fireworks and seasonal special events.
-            </p>
-          </details>
-        </section>
+        <main className={style.content}>
+          <section className={style.painel}>
+            <div className={style.painelHeader}>
+              <h1>Configurações - {option}</h1>
+            </div>
+            <div className={style.painelMain}>
+            <SettingsInterfaceMenu />
+            <SettingsInterfaceAnimation />
+            </div>
+          </section>
+        </main>
       </div>
     </Layout>
   );
+}
+
+
+export function SettingsInterfaceAnimation(){
+  return (
+    <div><span>interface</span></div>
+  )
 }

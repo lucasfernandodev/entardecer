0df6926c205feda3ae30c353a73aeeed346d8244
Lucas Fernando <lucasfernando.dev@gmail.com>
2022-10-02 04:08:06 -0300

    feat: add new icon & clear code

--- src/Components/Molecules/SettingsInterfaceBackground/index.tsx
+++ src/Components/Molecules/SettingsInterfaceBackground/index.tsx
@@ -3,14 +3,17 @@ import { message } from '../../../services/chrome/message';
 import { db } from '../../../storage/database';
 import blobToBase64 from '../../../utils/blobToBase64';
 import crop from '../../../utils/image/crop';
+import ButtonSwich from '../../Atoms/Buttons/Swich';
 import Icon from '../../utils/icon';
 import Alert from '../Alert';
 import style from './style.module.css';
 
 interface imagecroped {
-  data?: Blob,
-  crop: Boolean
+  data?: Blob;
+  crop: Boolean;
 }
+
+type Drag = React.DragEvent<HTMLDivElement>;
 
 export default function SettingsInterfaceBackground() {
   const [error, setError] = useState<string | null>(null);
@@ -32,131 +35,141 @@ export default function SettingsInterfaceBackground() {
     })();
   }, []);
 
-  async function storeImage(file: any) {
+  async function storeImage(file: File) {
+    setError('');
+    async function uploadImage(image64: string) {
+      const { bg_homepage: database } = await db();
 
-    const windowSize = {width: window.innerWidth,
-      height: window.innerHeight};
+      const isImage = await database.getAll('image');
 
-    console.log('store image initilized');
+      if (isImage.length === 0) {
+        await database.add('image', {
+          data: image64,
+          id: 'bg_homepage',
+        });
+      } else {
+        await database.put('image', {
+          data: image64,
+          id: 'bg_homepage',
+        });
+      }
+    }
+
+    const fileSize = file.size / 1024;
+    const windowSize = { width: window.innerWidth, height: window.innerHeight };
+    const imageTypesAccepts: string[] = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp', 'image/svg+xml']
+
+    if (fileSize > 3072) {
+      setError('- São aceitas somentes imagem de tamanho maximo 3MB');
+      setImage(null);
+      setIsUploadLoading(false);
+      setIsLoading(false)
+      return;
+    }
+
+    if(!imageTypesAccepts.includes(file.type)){
+      console.log(file.type)
+      console.log(imageTypesAccepts)
+      setError('- Arquivo de imagem invalido');
+      setImage(null);
+      setIsUploadLoading(false);
+      setIsLoading(false)
+      return;
+    }
+
     const imageUrl = URL.createObjectURL(file);
-    const imageCropped = await crop(imageUrl,windowSize)  as imagecroped
+    const isCropImage = (await crop(imageUrl, windowSize)) as imagecroped;
 
-    const imageInBase64 = imageCropped.data ? imageCropped.data : await blobToBase64(file);
+    const image64 = isCropImage.data
+      ? isCropImage.data
+      : await blobToBase64(file);
 
     try {
-      if (file) {
-        const { bg_homepage: database } = await db();
+      await uploadImage(image64 as string);
+      setIsUploadLoading(false);
+      setIsUploaded(true);
+      setImage(imageUrl);
 
-        const isImage = await database.getAll('image');
-
-        if (isImage.length === 0) {
-          await database.add('image', {
-            data: imageInBase64,
-            id: 'bg_homepage',
-          });
-        } else {
-          await database.put('image', {
-            data: imageInBase64,
-            id: 'bg_homepage',
-          });
-        }
-
-        setIsUploadLoading(false);
-        setIsUploaded(true);
-        setImage(imageUrl);
-        console.log('store image end');
-        
-       try {
-        const request= await message.send({
-          from: 'configuration',
-          to: 'homepage',
-          subject: 'update',
-        });
-
-       } catch (error) {
-        console.log(error)
-       }
-      }
+      await message.send({
+        from: 'configuration',
+        to: 'homepage',
+        subject: 'update',
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
-  function handleFile(file: any) {
-    const fileSize = file.size / 1024;
-
-    if (file) {
-      if (fileSize > 3072) {
-        setError('- São aceitas somentes imagem de tamanho maximo 3MB');
-        setImage(null);
-      } else {
-        storeImage(file);
-        setError(null);
-      }
-    }
-  }
-
-  function preverImage(evt: React.ChangeEvent<HTMLInputElement>) {
+  function handleOnChange(evt: React.ChangeEvent<HTMLInputElement>) {
     setIsUploadLoading(true);
     const input = evt.target as any;
     const file = input.files[0];
-    handleFile(file);
+    storeImage(file);
+    input.value = ''
   }
 
-  function dropHandler(ev: React.DragEvent<HTMLDivElement>) {
-    // Impedir o comportamento padrão (impedir que o arquivo seja aberto)
-    setIsUploadLoading(true);
-    ev.preventDefault();
-    const el = ev.target as HTMLElement;
+  function handleDrag() {
+    function processImage(e: Drag) {
+      let file;
 
-    el.classList.remove(style.dragOver);
-
-    if (ev.dataTransfer.items) {
-      // Use a interface DataTransferItemList para acessar o (s) arquivo (s)
-      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // Se os itens soltos não forem arquivos, rejeite-os
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          let file = ev.dataTransfer.items[i].getAsFile() as any;
-          handleFile(file);
+      if (e.dataTransfer.items) {
+        // Use a interface DataTransferItemList para acessar o (s) arquivo (s)
+        for (var i = 0; i < e.dataTransfer.items.length; i++) {
+          // Se os itens soltos não forem arquivos, rejeite-os
+          if (e.dataTransfer.items[i].kind === 'file') {
+            file = e.dataTransfer.items[i].getAsFile() as any;
+          }
+        }
+      } else {
+        // Use a interface DataTransfer para acessar o (s) arquivo (s)
+        for (let i = 0; i < e.dataTransfer.files.length; i++) {
+          file = e.dataTransfer.files[i];
         }
       }
-    } else {
-      // Use a interface DataTransfer para acessar o (s) arquivo (s)
-      for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-        const file = ev.dataTransfer.files[i];
-        handleFile(file);
-      }
+
+      storeImage(file);
     }
+
+    return {
+      over: (e: Drag) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const el = e.target as HTMLElement;
+
+        el.classList.add(style.dragOver);
+      },
+      drop: (e: Drag) => {
+        e.preventDefault();
+        setIsUploadLoading(true);
+
+        const el = e.target as HTMLElement;
+
+        el.classList.remove(style.dragOver);
+        processImage(e);
+      },
+      leave: (e: Drag) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const el = e.target as HTMLElement;
+
+        el.classList.remove(style.dragOver);
+      },
+    };
   }
 
-  function onDragOver(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault();
-    e.stopPropagation();
-    const el = e.target as HTMLElement;
-
-    el.classList.add(style.dragOver);
-  }
-
-  function onDragLeave(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault();
-    e.stopPropagation();
-    const el = e.target as HTMLElement;
-
-    el.classList.remove(style.dragOver);
-  }
 
   return (
-    <form className={style.uploadPhoto}>
+    <form className={style.form}>
       <h3>Alterar plano de fundo</h3>
       <p>Personalizar imagem de background da pagina inicial</p>
 
       <div
         className={style.preview}
-        onDragOver={onDragOver}
-        onDrop={dropHandler}
-        onDragLeave={onDragLeave}
+        onDragOver={handleDrag().over} 
+        onDrop={handleDrag().drop}
+        onDragLeave={handleDrag().leave}
         style={{ backgroundImage: `url(${image})` }}
-        data-loading-image={
+        data-loading={
           isUploadLoading || (isLoading && !isUploaded) ? true : false
         }
       >
@@ -165,20 +178,25 @@ export default function SettingsInterfaceBackground() {
             <div className={style.content}>
               <Icon name='image' className={style.icon} />
               <p>
-                Solte sua imagem aqui, ou{' '}
-                <label htmlFor={style.inputImage}>selecionar imagem</label>
+                <span>Solte sua imagem aqui, ou</span>
+                <label htmlFor={style.formInput}>selecionar imagem</label>
               </p>
             </div>
           </div>
         )}
+
+        {isUploaded && 
+        (<button className={style.btnUpdateImage} onClick={e => e.preventDefault()}>
+        <Icon name='update' />  
+      </button>)}
       </div>
 
       <p className={style.msgError}>{error}</p>
       <input
         type='file'
-        id={style.inputImage}
+        id={style.formInput}
         accept='image/*'
-        onChange={preverImage}
+        onChange={handleOnChange}
       />
       {isUploaded && (
         <Alert
@@ -187,6 +205,8 @@ export default function SettingsInterfaceBackground() {
           msg='O Background alterado com sucesso'
         />
       )}
+
+      
     </form>
   );
 }

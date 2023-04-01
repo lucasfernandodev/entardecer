5fe0ce63ebdf151382a7c0930208487e24fdf90b
Lucas Fernando <lucasfernando.dev@gmail.com>
2023-04-01 15:07:12 -0300

    build: add icon generate

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from "@crxjs/vite-plugin";
 import manifest from './manifest.config';
+import vitePluginFaviconInject from 'vite-plugin-favicons-inject'
 
 const root = resolve(__dirname, 'src');
-const outDir = '../dist';
 
+// exit src folder
+const outDir = '../dist/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), vitePluginFaviconInject('./src/public/images/logo.svg')],
   build: {
     outDir,
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(root, 'pages', 'popup', 'index.html'),
